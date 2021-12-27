d00008d760acd8e47565bef36c97a669e2ce4607
Eduardo Lamarche <lamarcheeduardo@gmail.com>
2021-12-26 21:47:09 -0400

    first Round is Working. Tomorrow will try with the loop

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+//computerPlay function
+
 function computerPlay() {
-    console.log(result);
+    const choices = ["rock", "paper", "scissors"];
+    let result = Math.floor(Math.random()*3);
+    return choices[result];
 }
+
+function playRound() {
+
+    let playerSelection = prompt("Pick one: Rock, Paper or Scissors");
+    let computerSelection = computerPlay();
+    let ps = playerSelection.toLowerCase();
+    let cs = computerSelection.toLowerCase();
+
+    if (ps === cs) {
+        return "It's a Tie";
+    } else if (ps === "rock") {
+        if (cs === "paper") {
+            return "You lost. Paper Beats Rock";
+        } else if (cs === "scissors") {
+            return "You Won, Rock wins over Scissors";
+        }
+
+    } else if (ps === "paper") {
+        if (cs === "rock") {
+            return "You win. Paper Wins over Rock";
+        } else if (cs === "scissors") {
+            return "You lost, scissors wins over paper"
+        }
+    } else if (ps === "scissors") {
+        if (cs === "rock") {
+            return "You lost. Rock Wins over Scissors"; 
+        } else if (cs === "paper") {
+            return "You won. Scissors win over Paper"
+        }
+    }
+
+}
+
+console.log(playRound());
+
